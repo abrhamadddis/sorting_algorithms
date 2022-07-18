bdf1c7526b1b8ec998b3b7936bad0f9de4c938c6
Xmuluneh <mulabr673@gmail.com>
2022-07-18 18:21:33 +0300

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,73 +1,93 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * quick_sort - function call quicksort
- * @array :the array to be sorted
- * @size :the size of the array
- * @return the index of the pivot
+ * _swap - swaps two values in an array
+ *
+ * @array: data to sort
+ * @i: first value
+ * @j: second value
+ * @size: size of data
+ *
+ * Return: No Return
+ */
+void _swap(int *array, int i, int j, int size)
+{
+	int tmp;
+
+	if (array[i] != array[j])
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		print_array(array, size);
+	}
+}
+
+/**
+ * partition - sorts a partition of data in relation to a pivot
+ *
+ * @array: data to sort
+ * @min: Left wall
+ * @max: right wall
+ * @size: size of data
+ *
+ * Return: New Pivot
+ */
+int partition(int *array, int min, int max, size_t size)
+{
+	int i = min, j, pivot  = array[max];
+
+	for (j = min; j <= max; j++)
+	{
+		if (array[j] < pivot)
+		{
+			_swap(array, i, j, size);
+			i++;
+		}
+
+	}
+	_swap(array, i, max, size);
+
+	return (i);
+}
+
+/**
+ * quicksort -  sorts an array of integers in ascending order using the
+ * Quick sort algorithm Lomuto partition scheme
+ *
+ * @array: data to sort
+ * @min: Left wall
+ * @max: right wall
+ * @size: size of data
+ *
+ * Return: No Return
+ */
+void quicksort(int *array, int min, int max, size_t size)
+{
+	int p;
+
+	if (min < max)
+	{
+		p = partition(array, min, max, size);
+		quicksort(array, min, p - 1, size);
+		quicksort(array, p + 1, max, size);
+	}
+}
+
+/**
+ * quick_sort -  sorts an array of integers in ascending order using the
+ * Quick sort algorithm Lomuto partition scheme
+ *
+ * @array: data to sort
+ * @size: size of data
+ *
+ * Return: No Return
  */
 void quick_sort(int *array, size_t size)
 {
-if (!array || !size)
-return;
-quicksort(array, size, 0, size - 1);
+	if (size < 2)
+		return;
 
-
-}
-/**
- * quicksort - function via partition schemes
- * @size: the size of the array
- * @array:the array to be sorted
- * @first :the index of the first element in the array
- * @last :the last index of the array
- */
-void  quicksort(int *array, size_t size, size_t first, size_t last)
-{
-if (last > first)
-{
-int pivotIndex = partition(array, size,  first, last);
-quicksort(array, size, first, pivotIndex - 1);
-quicksort(array, size, pivotIndex + 1, last);
-}
-}
-/**
- * partition - function partition the array
- * @array : the array to be sorted
- * @size: the size of the array
- * @first : the first index of the array
- * @last :the last index of the array
- * Return :void
- */
-int partition(int *array, size_t size, size_t first, size_t last)
-{
-int pivot = array[first];
-int low = first + 1;
-int high = last;
-while (high > low)
-{
-while (low <= high && array[low] <= pivot)
-	low++;
-while (low <= high && array[high] > pivot)
-	high--;
-if (high > low)
-{
-int temp = array[high];
-array[high] = array[low];
-array[low] = temp;
-
-}
-}
-while (high > first && array[high] >= pivot)
-	high--;
-if (pivot > array[high])
-{
-array[first] = array[high];
-array[high] = pivot;
-return (high);
-}
-else
-{
-return (first);
-}
-
+	quicksort(array, 0, size - 1, size);
 }
