b06be82676be5033939d1c18d50f750e10f67117
abrhamadddis <abrhamaddis32@gmail.com>
2022-07-19 09:37:41 +0300

    mandatory task

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,23 +1,29 @@
 #include "sort.h"
 /**
- * selection_sort - a function that use selection sorthing algorthum
- * @array: is a pointer to an array
- * @size: is an int that represent the size of an array
+ * selection_sort - sorts a list using selection sort
+ *
+ * @array: The list to be sorted
+ * @size: The size of the list
  */
 void selection_sort(int *array, size_t size)
 {
-int i, key, j;
-int n = size;
-for (i = 1; i < n; i++)
-{
-key = array[i];
-j = i - 1;
-while (j >= 0 && array[j] > key)
-{
-array[j + 1] = array[j];
-j = j - 1;
-}
-array[j + 1] = key;
-print_array(array, size);
-}
+	size_t i = 0, j = 0, m = 0, t = 0;
+
+	if (array == NULL || size == 0)
+		return;
+	for ( ; i < size - 1; i++)
+	{
+		m = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[m])
+				m = j;
+		}
+		if (array[m] == array[i])
+			continue;
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+		print_array(array, size);
+	}
 }
